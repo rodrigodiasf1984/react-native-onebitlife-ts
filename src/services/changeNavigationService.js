e2c757aff0fe6ea5f740bdcdd4acefdc7d8788aa
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-01-03 23:03:35 +0100

    chore: add setHowHome service

--- src/services/changeNavigationService.js
+++ src/services/changeNavigationService.js
@@ -9,3 +9,22 @@ db.transaction(tx => {
     },
   );
 });
+
+export const setShowHome = obj => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'INSERT INTO change_navigation (showHome, appStartData) values (?, ?);',
+        [obj.showHome, obj.appStartData],
+        (_, { rowsAffected, insertId }) => {
+          if (rowsAffected > 0) {
+            resolve(insertId);
+          }
+        },
+        (_, error) => {
+          reject(error);
+        },
+      );
+    });
+  });
+};
