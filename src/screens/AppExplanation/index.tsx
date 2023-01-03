eabf59332894584df558e440e10304373999fdca
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-01-03 23:36:01 +0100

    chore: add checkShowHome service

--- src/screens/AppExplanation/index.tsx
+++ src/screens/AppExplanation/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 import { navigationProps } from '@types';
 
 import DefaultButton from '~/components/common/DefaultButton';
 import ExplanationCard from '~/components/ExplanationCard';
+import ChangeNavigationService from '~/services/changeNavigationService.js';
 
 import {
   CustomSafeAreaView,
@@ -15,6 +16,21 @@ import {
 
 export default function AppExplanation() {
   const navigation = useNavigation<navigationProps>();
+  const [showHome, setShowHome] = useState('false');
+  const startDate = new Date();
+  const appStartData = `${startDate.getFullYear()}-${startDate.getMonth()}-${startDate.getDay()}`;
+
+  function handleSetShowHome() {
+    console.log('✅ ~  showHome', showHome);
+    if (showHome !== 'true') {
+      ChangeNavigationService.setShowHome({ showHome: 'true', appStartData })
+        .then(() => console.log(`Sucesso! ${showHome} ${appStartData}`))
+        .catch(err => console.log(err));
+      setShowHome('true');
+      navigation.navigate('Home');
+    }
+  }
+
   return (
     <CustomSafeAreaView>
       <Title>Antes, deixa {'\n'} eu te explicar...</Title>
@@ -28,7 +44,7 @@ export default function AppExplanation() {
         buttonText="Continuar"
         width={250}
         height={50}
-        onPress={() => navigation.navigate('Home')}
+        onPress={() => handleSetShowHome()}
       />
     </CustomSafeAreaView>
   );
